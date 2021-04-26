ed30e2b22d92ea48a58e0459457579fa3f4448bc
Kartofelni <mp2000panin@gmail.com>
2021-04-26 21:08:08 +0300

    First commit

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = "Лох помойный"
-    return render_template('index.html', title='Домашняя страница',
+    return render_template('base.html', title='Домашняя страница',
                            username=user)
 
 
-if __name__ == '__main__':
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+def main():
     app.run(port=8080, host='127.0.0.1')
+
+
+if __name__ == '__main__':
+    main()
