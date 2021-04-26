808f812b03cedf3f9066b332b0126bfbbb0a8584
Kartofelni <mp2000panin@gmail.com>
2021-04-26 21:37:21 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template, redirect
 from loginform import LoginForm
+from data import db_session
+from data.users import User
+from data.notes import Note
+from forms.user import RegisterForm
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    user = "Лох помойный"
-    return render_template('base.html', title='Домашняя страница',
-                           username=user)
+    db_sess = db_session.create_session()
+    news = db_sess.query(Note).filter(Note.is_private != True)
+    return render_template("index.html", news=news)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -21,7 +25,33 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 def main():
+    db_session.global_init("db/blogs.db")
     app.run(port=8080, host='127.0.0.1')
 
 
